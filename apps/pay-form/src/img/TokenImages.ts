9b14e76c6488b18ac993ae46fdff9f6c4cbe8d74
melonges <mellonges@icloud.com>
2023-06-25 21:34:05 +0300

    feat(asset): add new asset

--- apps/pay-form/src/img/TokenImages.ts
+++ apps/pay-form/src/img/TokenImages.ts
@@ -1,14 +1,51 @@
-import bnb from "./bnb.svg";
-import btc from "./btc.svg";
-import dai from "./dai.svg";
-import busd from "./busd.svg";
-import matic from "./matic.svg";
-import usdc from "./usdc.svg";
-import usdt from "./usdt.svg";
-import weth from "./weth.svg";
-import ltc from "./litecoin.svg";
-import doge from "./Dogecoin.svg";
-import { Asset_t } from "../store/api/endpoints/types";
+import bnb from './bnb.svg';
+import btc from './btc.svg';
+import dai from './dai.svg';
+import busd from './busd.svg';
+import matic from './matic.svg';
+import usdc from './usdc.svg';
+import usdt from './usdt.svg';
+import weth from './weth.svg';
+import ltc from './litecoin.svg';
+import doge from './Dogecoin.svg';
+import aave from './aave.svg';
+import ape from './ape.svg';
+import bch from './bitcoin-cash.svg';
+import bitdao from './bitdao.svg';
+import btt from './bittorrent.svg';
+import chz from './chiliZ.png';
+import crv from './crv.svg';
+import egld from './elrond.png';
+import eos from './eos.svg';
+import etc from './etc.svg';
+import fil from './filecoin.svg';
+import flow from './flow.svg';
+import gala from './gala.svg';
+import grt from './grt.svg';
+import gusd from './gusd.svg';
+import ht from './HuobiToken.svg';
+import inj from './inj.svg';
+import kava from './kava.svg';
+import ldo from './ldo.svg';
+import leo from './leo.svg';
+import link from './link.svg';
+import mana from './mana.svg';
+import mkr from './mkr.svg';
+import near from './near.svg';
+import okb from './okb.svg';
+import paxg from './paxg.svg';
+import polkadot from './polkadot.svg';
+import qnt from './qnt.svg';
+import rndr from './rndr.png';
+import rpl from './rpl.svg';
+import sand from './sand.svg';
+import tusd from './tusd.svg';
+import uni from './uni.svg';
+import usdd from './usdd.svg';
+import shib from './shib.svg';
+import zec from './zec.svg';
+import snx from './snx.svg';
+import { Asset_t } from '../store/api/endpoints/types';
 
 export type TokenImagesType = {
   [key in Asset_t]: string;
@@ -23,10 +60,46 @@ export const TokenImages: Readonly<TokenImagesType> = {
   usdt,
   btc,
   eth: weth,
-  trx: "",
   wbtc: btc,
   weth,
   wmatic: matic,
   ltc,
-  doge
+  doge,
+  aave,
+  ape,
+  bch,
+  bit: bitdao,
+  btt,
+  chz,
+  crv,
+  egld,
+  eos,
+  etc,
+  fil,
+  flow,
+  gala,
+  grt,
+  gusd,
+  ht,
+  inj,
+  kava,
+  ldo,
+  leo,
+  link,
+  mana,
+  mkr,
+  near,
+  okb,
+  paxg,
+  polkadot,
+  qnt,
+  rndr,
+  rpl,
+  sand,
+  tusd,
+  uni,
+  usdd,
+  shib,
+  zec,
+  snx,
 };
--- apps/pay-form/src/store/api/endpoints/types.ts
+++ apps/pay-form/src/store/api/endpoints/types.ts
@@ -1,28 +1,68 @@
 export type Blockchain_t =
-  | "arbitrum"
-  | "bsc"
-  | "ethereum"
-  | "polygon"
-  | "optimism"
-  | "tron"
-  | "bitcoin"
-  | "dogecoin"
-  | "litecoin";
+  | 'arbitrum'
+  | 'bsc'
+  | 'ethereum'
+  | 'polygon'
+  | 'optimism'
+  | 'tron'
+  | 'bitcoin'
+  | 'dogecoin'
+  | 'litecoin'
+  | 'bitcoin-cash';
 export type Asset_t =
-  | "usdt"
-  | "usdc"
-  | "dai"
-  | "busd"
-  | "matic"
-  | "eth"
-  | "bnb"
-  | "trx"
-  | "wbtc"
-  | "weth"
-  | "wmatic"
-  | "btc"
-  | "ltc"
-  | "doge";
+  | 'usdt'
+  | 'usdc'
+  | 'dai'
+  | 'busd'
+  | 'matic'
+  | 'eth'
+  | 'bnb'
+  | 'wbtc'
+  | 'weth'
+  | 'wmatic'
+  | 'btc'
+  | 'ltc'
+  | 'doge'
+  | 'aave'
+  | 'ape'
+  | 'bch'
+  | 'bit'
+  | 'btt'
+  | 'chz'
+  | 'crv'
+  | 'egld'
+  | 'eos'
+  | 'etc'
+  | 'fil'
+  | 'flow'
+  | 'gala'
+  | 'grt'
+  | 'gusd'
+  | 'ht'
+  | 'inj'
+  | 'kava'
+  | 'ldo'
+  | 'leo'
+  | 'link'
+  | 'mana'
+  | 'mkr'
+  | 'near'
+  | 'okb'
+  | 'paxg'
+  | 'polkadot'
+  | 'qnt'
+  | 'rndr'
+  | 'rpl'
+  | 'sand'
+  | 'shib'
+  | 'snx'
+  | 'tusd'
+  | 'uni'
+  | 'usdd'
+  | 'wbtc'
+  | 'weth'
+  | 'wmatic'
+  | 'zec';
 
 type ChainIdEnumUnion = 1 | 56 | 137 | 42161 | -1 | -2 | 10;
 
@@ -36,26 +76,27 @@ export const ChainId: { [key in Blockchain_t]: number } = {
   bitcoin: -1,
   litecoin: -1,
   dogecoin: -1,
+  'bitcoin-cash': -1,
 };
 
 export const ChainIdToName: { [key in ChainIdEnumUnion]: Blockchain_t } = {
-  1: "ethereum",
-  56: "bsc",
-  137: "polygon",
-  42161: "arbitrum",
-  10: "optimism",
-  "-2": "tron",
-  "-1": "bitcoin",
+  1: 'ethereum',
+  56: 'bsc',
+  137: 'polygon',
+  42161: 'arbitrum',
+  10: 'optimism',
+  '-2': 'tron',
+  '-1': 'bitcoin',
 };
 
 export const WrappedToken: { [key in Asset_t]?: Asset_t } = {
-  btc: "wbtc",
-  eth: "weth",
-  matic: "wmatic",
+  btc: 'wbtc',
+  eth: 'weth',
+  matic: 'wmatic',
 };
 
 export const WrappedTokenToToken: { [key in Asset_t]?: Asset_t } = {
-  wbtc: "btc",
-  weth: "eth",
-  wmatic: "matic",
+  wbtc: 'btc',
+  weth: 'eth',
+  wmatic: 'matic',
 };
