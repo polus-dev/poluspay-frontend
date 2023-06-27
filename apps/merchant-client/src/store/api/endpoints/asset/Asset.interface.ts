3936b2ce2f82c9a63d1eeff0f2283f2514e413db
melonges <mellonges@icloud.com>
2023-06-27 16:36:34 +0300

    feat(merchant-client): available asset

--- apps/merchant-client/src/store/api/endpoints/asset/Asset.interface.ts
+++ apps/merchant-client/src/store/api/endpoints/asset/Asset.interface.ts
@@ -1,13 +1,15 @@
-import { Asset_t, Blockchain_t } from "../../types";
+import { Asset_t, Blockchain_t } from '../../types';
 
 export type IAssetsResponse = {
     [key in Asset_t]: {
-        [key in Blockchain_t]: {
-            is_native: boolean;
-            contract: string;
-            decimals: number;
-            is_seeded_amount: boolean;
-            available_for_accept: boolean;
+        networks: {
+            [key in Blockchain_t]: {
+                is_native: boolean;
+                contract: string;
+                decimals: number;
+                is_seeded_amount: boolean;
+                available_for_accept: boolean;
+            };
         };
     };
 };
