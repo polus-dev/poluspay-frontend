7c3f171e66d9496ba8843599e33852a6e037ada4
melonges <mellonges@icloud.com>
2023-06-30 01:19:13 +0300

    fix(dependency): installed & removed

--- libs/redux/README.md
+++
@@ -1,7 +0,0 @@
-# redux
-
-This library was generated with [Nx](https://nx.dev).
-
-## Building
-
-Run `nx build redux` to build the library.
--- libs/ui/src/hooks/useNotify.ts
+++ libs/ui/src/hooks/useNotify.ts
@@ -2,40 +2,40 @@ import { PLabel, LabelProps } from '@poluspay-frontend/ui';
 import { useState } from 'react';
 import * as ReactDOM from 'react-dom';
 interface NotifyProps extends Omit<LabelProps, 'visible'> {
-  timeOut?: number;
+    timeOut?: number;
 }
 
 type LabelStatus = 'success' | 'error' | 'warning';
 export const useNotify = () => {
-  const [isVisible, setIsVisible] = useState(false);
-  const [status, setStatus] = useState<LabelStatus>('success');
-  const [loading, setLoading] = useState(false);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+    const [isVisible, setIsVisible] = useState(false);
+    const [status, setStatus] = useState<LabelStatus>('success');
+    const [loading, setLoading] = useState(false);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
 
-  ReactDOM.createPortal(
-    PLabel({
-      visible: isVisible,
-      status,
-      description,
-      title,
-      loading,
-    }),
-    document.body
-  );
+    ReactDOM.createPortal(
+        PLabel({
+            visible: isVisible,
+            status,
+            description,
+            title,
+            loading,
+        }),
+        document.body
+    );
 
-  const notify = (args: NotifyProps) => {
-    setIsVisible(true);
-    setStatus(args.status || 'success');
-    setLoading(args.loading || false);
-    setTitle(args.title || '');
-    setDescription(args.description || '');
-    setTimeout(() => {
-      setIsVisible(false);
-    }, args.timeOut || 3000);
-  };
+    const notify = (args: NotifyProps) => {
+        setIsVisible(true);
+        setStatus(args.status || 'success');
+        setLoading(args.loading || false);
+        setTitle(args.title || '');
+        setDescription(args.description || '');
+        setTimeout(() => {
+            setIsVisible(false);
+        }, args.timeOut || 3000);
+    };
 
-  return {
-    notify,
-  };
+    return {
+        notify,
+    };
 };
