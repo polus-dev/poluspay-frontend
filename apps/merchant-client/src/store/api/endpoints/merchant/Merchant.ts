92996febfc34c637b9dee7d111b6caa23eefdfa2
melonges <mellonges@icloud.com>
2023-07-01 14:39:00 +0300

    fix(merchant-client): typo

--- apps/merchant-client/src/store/api/endpoints/merchant/Merchant.ts
+++ apps/merchant-client/src/store/api/endpoints/merchant/Merchant.ts
@@ -147,5 +147,5 @@ export const {
     useGetMerchantsQuery,
     useGetWebhookHistoryQuery,
     useGetMerchantByIdQuery,
-    eVerifyDomainMutation,
+    useVerifyDomainMutation,
 } = merchantApi;
