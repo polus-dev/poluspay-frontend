6116fa05dca54b3c0b8c34cb201cb85e9e5b87ff
Nick Deev <89545245+22Crusader@users.noreply.github.com>
2023-07-06 20:24:20 +0300

    feat(merchant-client): invoices table pagination

--- apps/merchant-client/src/pages/merchants/id/invoices/MerchantInvoices.tsx
+++ apps/merchant-client/src/pages/merchants/id/invoices/MerchantInvoices.tsx
@@ -1,5 +1,8 @@
 import type { Invoice } from '../../../../components/pages/merchants/id/invoices/Table';
 
+import { useState } from 'react';
+
+import { PPagination } from '@poluspay-frontend/ui';
 import { MerchantInvoicesForm } from '../../../../components/pages/merchants/id/invoices/Form';
 import { MerchantInvoicesTable } from '../../../../components/pages/merchants/id/invoices/Table';
 import { MerchantInvoicesPreview } from '../../../../components/pages/merchants/id/invoices/Preview';
@@ -50,6 +53,19 @@ export const MerchantInvoicesPage: React.FC = () => {
         },
     ];
 
+    const [current, setCurrent] = useState(1);
+    const limit = 10;
+
+    const invoicesPaginated =
+        invoices.slice(
+            (current - 1) * limit,
+            (current - 1) * limit + limit
+        );
+
+    const onPageChange = (value: number) => {
+        setCurrent(value);
+    };
+
     return (
         <div className="invoices">
             <div className="invoices__inner">
@@ -66,8 +82,18 @@ export const MerchantInvoicesPage: React.FC = () => {
             <div className="invoices__table">
                 <h6 className="invoices__table-title">Invoices</h6>
                 <div className="invoices__table-container">
-                    <MerchantInvoicesTable invoices={invoices} />
+                    <MerchantInvoicesTable invoices={invoicesPaginated} />
                 </div>
+                {invoices.length > limit && (
+                    <div className="invoices__table-pagination">
+                        <PPagination
+                            current={current}
+                            totalItems={invoices.length}
+                            pageItems={limit}
+                            onPageChange={(value) => onPageChange(value)}
+                        />
+                    </div>
+                )}
             </div>
         </div>
     );
