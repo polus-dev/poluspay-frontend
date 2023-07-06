bf80efe01009c4d596a31618779271bb80f33c71
melonges <mellonges@icloud.com>
2023-07-06 12:20:08 +0300

    fix(ui lib): props error adapter for input

--- libs/ui/src/lib/FormInput/FormInput.tsx
+++ libs/ui/src/lib/FormInput/FormInput.tsx
@@ -1,42 +1,48 @@
+import { useEffect } from 'react';
 import { InputProps, PInput } from '../PInput/PInput';
 
 import './FormInput.scoped.scss';
+import { error } from 'console';
 
 interface FormInputProps extends InputProps {
-    label?: string;
-    underlabel?: string;
+  label?: string;
+  underlabel?: string;
+  error?: string;
 }
 
 export const FormInput: React.FC<FormInputProps> = ({
-    label,
-    underlabel,
-    ...props
+  label,
+  underlabel,
+  ...props
 }) => {
-    return (
-        <div className="form-input">
-            {label ||
-                (props.errors?.length && (
-                    <div className="form-input__data">
-                        {label && (
-                            <p className="form-input__data-label">{label}</p>
-                        )}
-                        {props.errors.length && (
-                            <p className="form-input__data-label form-input__data-label--error">
-                                {props.errors[0]}
-                            </p>
-                        )}
-                    </div>
-                ))}
-            <div className="form-input__element">
-                <PInput {...props} />
-            </div>
-            {underlabel && (
-                <div className="form-input__data">
-                    <p className="form-input__data-label form-input__data-label--dark">
-                        {underlabel}
-                    </p>
-                </div>
+  return (
+    <div className="form-input">
+      {label ||
+        (props.error && (
+          <div className="form-input__data">
+            {label && (
+              <p className="form-input__data-label">{label}</p>
             )}
+            {props.error && (
+              <p className="form-input__data-label form-input__data-label--error">
+                {props.error}
+              </p>
+            )}
+          </div>
+        ))}
+      <div className="form-input__element">
+        <PInput
+          {...props}
+          errors={props.error ? [props.error] : undefined}
+        />
+      </div>
+      {underlabel && (
+        <div className="form-input__data">
+          <p className="form-input__data-label form-input__data-label--dark">
+            {underlabel}
+          </p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
