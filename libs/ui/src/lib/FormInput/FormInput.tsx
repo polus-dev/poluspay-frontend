4a8c4c96062978bb2ede3b979da56c861700931c
Nick Deev <89545245+22Crusader@users.noreply.github.com>
2023-07-07 09:10:21 +0300

    feat(ui-libs): FormInput asterisk & labelSizes props

--- libs/ui/src/lib/FormInput/FormInput.tsx
+++ libs/ui/src/lib/FormInput/FormInput.tsx
@@ -1,48 +1,66 @@
-import { useEffect } from 'react';
 import { InputProps, PInput } from '../PInput/PInput';
 
+import classNames from 'classnames';
+
 import './FormInput.scoped.scss';
-import { error } from 'console';
+
+type LabelSize = 'sm' | 'md' | 'lg'
 
 interface FormInputProps extends InputProps {
-  label?: string;
-  underlabel?: string;
-  error?: string;
+    label?: string;
+    labelSize?: LabelSize
+    asterisk?: boolean
+    underlabel?: string;
+    error?: string;
 }
 
 export const FormInput: React.FC<FormInputProps> = ({
-  label,
-  underlabel,
-  ...props
+    label,
+    labelSize = 'sm',
+    asterisk,
+    underlabel,
+    ...props
 }) => {
-  return (
-    <div className="form-input">
-      {label ||
-        (props.error && (
-          <div className="form-input__data">
-            {label && (
-              <p className="form-input__data-label">{label}</p>
+    return (
+        <div className="form-input">
+            {label ||
+                (props.error && (
+                    <div className="form-input__data">
+                        {label && (
+                            <p
+                                className={classNames({
+                                    'form-input__data-label': true,
+                                    [`form-input__data-label--${labelSize}`]: true
+                                })}
+                            >
+                                {label}
+                                {asterisk && (
+                                    <span className="form-input__data-label-asterisk">
+                                        &nbsp;*
+                                    </span>
+                                )}
+                            </p>
+                        )}
+                        {props.error && (
+                            <p className="form-input__data-label form-input__data-label--error">
+                                {props.error}
+                            </p>
+                        )}
+                    </div>
+                ))}
+            <div className="form-input__element">
+                <PInput
+                    {...props}
+                    errors={props.error ? [props.error] : undefined}
+                />
+            </div>
+            {underlabel && (
+                <div className="form-input__data">
+                    <p className="form-input__data-label form-input__data-label--dark">
+                        {underlabel}
+                    </p>
+                </div>
             )}
-            {props.error && (
-              <p className="form-input__data-label form-input__data-label--error">
-                {props.error}
-              </p>
-            )}
-          </div>
-        ))}
-      <div className="form-input__element">
-        <PInput
-          {...props}
-          errors={props.error ? [props.error] : undefined}
-        />
-      </div>
-      {underlabel && (
-        <div className="form-input__data">
-          <p className="form-input__data-label form-input__data-label--dark">
-            {underlabel}
-          </p>
         </div>
-      )}
-    </div>
-  );
+    );
 };
