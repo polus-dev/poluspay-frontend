f062bbc9a824cee5b3054cb60f038abbd0ec3d12
melonges <mellonges@icloud.com>
2023-07-08 22:03:25 +0300

    refactor(pay-form): typed contract abi

--- apps/pay-form/src/polus_abi.ts
+++ apps/pay-form/src/polus_abi.ts
@@ -1,22 +1,6 @@
 export default [
     {
-        inputs: [
-            {
-                internalType: 'address[]',
-                name: 'allowedInputs_',
-                type: 'address[]',
-            },
-            {
-                internalType: 'address',
-                name: 'feesRecipient_',
-                type: 'address',
-            },
-            {
-                internalType: 'uint256',
-                name: 'feePercentage_',
-                type: 'uint256',
-            },
-        ],
+        inputs: [],
         stateMutability: 'nonpayable',
         type: 'constructor',
     },
@@ -25,42 +9,48 @@ export default [
         inputs: [
             {
                 indexed: true,
+                internalType: 'bytes16',
+                name: 'uuid',
+                type: 'bytes16',
+            },
+            {
+                indexed: true,
                 internalType: 'address',
-                name: 'from',
+                name: 'token',
+                type: 'address',
+            },
+            {
+                indexed: true,
+                internalType: 'address',
+                name: 'sender',
                 type: 'address',
             },
             {
                 indexed: false,
                 internalType: 'address',
-                name: 'tokenIn',
-                type: 'address',
-            },
-            {
-                indexed: false,
-                internalType: 'address',
-                name: 'tokenOut',
-                type: 'address',
-            },
-            {
-                indexed: false,
-                internalType: 'address',
-                name: 'recipient',
+                name: 'feerecvr',
                 type: 'address',
             },
             {
                 indexed: false,
                 internalType: 'uint256',
-                name: 'amountIn',
+                name: 'feevalue',
                 type: 'uint256',
             },
             {
                 indexed: false,
-                internalType: 'bytes16',
-                name: 'uniqid',
-                type: 'bytes16',
+                internalType: 'address',
+                name: 'mrhrecvr',
+                type: 'address',
+            },
+            {
+                indexed: false,
+                internalType: 'uint256',
+                name: 'mrhvalue',
+                type: 'uint256',
             },
         ],
-        name: 'Action',
+        name: 'ERC20Payment',
         type: 'event',
     },
     {
@@ -68,132 +58,51 @@ export default [
         inputs: [
             {
                 indexed: true,
-                internalType: 'address',
-                name: 'previousOwner',
-                type: 'address',
+                internalType: 'bytes16',
+                name: 'uuid',
+                type: 'bytes16',
             },
             {
                 indexed: true,
                 internalType: 'address',
-                name: 'newOwner',
+                name: 'sender',
                 type: 'address',
             },
+            {
+                indexed: false,
+                internalType: 'address',
+                name: 'feerecvr',
+                type: 'address',
+            },
+            {
+                indexed: false,
+                internalType: 'uint256',
+                name: 'feevalue',
+                type: 'uint256',
+            },
+            {
+                indexed: false,
+                internalType: 'address',
+                name: 'mrhrecvr',
+                type: 'address',
+            },
+            {
+                indexed: false,
+                internalType: 'uint256',
+                name: 'mrhvalue',
+                type: 'uint256',
+            },
         ],
-        name: 'OwnershipTransferred',
+        name: 'ETHPayment',
         type: 'event',
     },
     {
         inputs: [
             {
-                internalType: 'address',
-                name: 'input',
-                type: 'address',
+                internalType: 'bytes16',
+                name: 'uuid',
+                type: 'bytes16',
             },
-        ],
-        name: 'allowedInputs',
-        outputs: [
-            {
-                internalType: 'bool',
-                name: '',
-                type: 'bool',
-            },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-    },
-    {
-        inputs: [
-            {
-                internalType: 'uint256',
-                name: 'amount',
-                type: 'uint256',
-            },
-        ],
-        name: 'calculateFee',
-        outputs: [
-            {
-                internalType: 'uint256',
-                name: '',
-                type: 'uint256',
-            },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-    },
-    {
-        inputs: [
-            {
-                internalType: 'address',
-                name: 'input',
-                type: 'address',
-            },
-        ],
-        name: 'disableAllowedInput',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-    },
-    {
-        inputs: [
-            {
-                internalType: 'address',
-                name: 'input',
-                type: 'address',
-            },
-        ],
-        name: 'enableAllowedInput',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-    },
-    {
-        inputs: [],
-        name: 'feePercentage',
-        outputs: [
-            {
-                internalType: 'uint256',
-                name: '',
-                type: 'uint256',
-            },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-    },
-    {
-        inputs: [],
-        name: 'owner',
-        outputs: [
-            {
-                internalType: 'address',
-                name: '',
-                type: 'address',
-            },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-    },
-    {
-        inputs: [],
-        name: 'renounceOwnership',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-    },
-    {
-        inputs: [],
-        name: 'router',
-        outputs: [
-            {
-                internalType: 'address',
-                name: '',
-                type: 'address',
-            },
-        ],
-        stateMutability: 'pure',
-        type: 'function',
-    },
-    {
-        inputs: [
             {
                 internalType: 'address',
                 name: 'token',
@@ -201,21 +110,26 @@ export default [
             },
             {
                 internalType: 'address',
-                name: 'recipient',
+                name: 'feerecvr',
                 type: 'address',
             },
             {
                 internalType: 'uint256',
-                name: 'amount',
+                name: 'feevalue',
                 type: 'uint256',
             },
             {
-                internalType: 'bytes16',
-                name: 'uniqid',
-                type: 'bytes16',
+                internalType: 'address',
+                name: 'mrhrecvr',
+                type: 'address',
+            },
+            {
+                internalType: 'uint256',
+                name: 'mrhvalue',
+                type: 'uint256',
             },
         ],
-        name: 'swapEqualInOutToken',
+        name: 'DoERC20Payment',
         outputs: [],
         stateMutability: 'nonpayable',
         type: 'function',
@@ -223,76 +137,34 @@ export default [
     {
         inputs: [
             {
-                internalType: 'address',
-                name: 'tokenIn',
-                type: 'address',
-            },
-            {
-                internalType: 'address',
-                name: 'tokenOut',
-                type: 'address',
-            },
-            {
-                internalType: 'address',
-                name: 'recipient',
-                type: 'address',
-            },
-            {
-                internalType: 'uint256',
-                name: 'amountIn',
-                type: 'uint256',
-            },
-            {
                 internalType: 'bytes16',
-                name: 'uniqid',
+                name: 'uuid',
                 type: 'bytes16',
             },
             {
-                internalType: 'uint24',
-                name: 'poolFee',
-                type: 'uint24',
-            },
-        ],
-        name: 'swapExactInputSingleHop',
-        outputs: [
-            {
-                internalType: 'uint256',
-                name: '',
-                type: 'uint256',
-            },
-        ],
-        stateMutability: 'nonpayable',
-        type: 'function',
-    },
-    {
-        inputs: [
-            {
                 internalType: 'address',
-                name: 'newOwner',
-                type: 'address',
-            },
-        ],
-        name: 'transferOwnership',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-    },
-    {
-        inputs: [
-            {
-                internalType: 'address',
-                name: 'feesRecipient_',
+                name: 'feerecvr',
                 type: 'address',
             },
             {
                 internalType: 'uint256',
-                name: 'feePercentage_',
+                name: 'feevalue',
+                type: 'uint256',
+            },
+            {
+                internalType: 'address',
+                name: 'mrhrecvr',
+                type: 'address',
+            },
+            {
+                internalType: 'uint256',
+                name: 'mrhvalue',
                 type: 'uint256',
             },
         ],
-        name: 'updateFeeConfiguration',
+        name: 'DoETHPayment',
         outputs: [],
-        stateMutability: 'nonpayable',
+        stateMutability: 'payable',
         type: 'function',
     },
 ] as const;
