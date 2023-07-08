2c8b909ed2c99250417579def8b287e308071a90
Nick Deev <89545245+22Crusader@users.noreply.github.com>
2023-07-08 14:32:18 +0300

    feat(merchant-client): created & integrated loading&error blocks

---
+++ apps/merchant-client/src/components/ui/Error/Error.tsx
@@ -0,0 +1,43 @@
+import classNames from 'classnames'
+
+import './Error.scoped.scss'
+
+interface ErrorProps {
+    title?: string
+    height?: number
+    borderRadius?: number
+    hasBackground?: boolean
+    largeTitle?: boolean
+}
+
+export const ErrorBlock: React.FC<ErrorProps> = ({
+    title = 'No records found',
+    height,
+    borderRadius,
+    hasBackground = true,
+    largeTitle
+}) => {
+    return (
+        <div
+            className={classNames({
+                'error': true,
+                'error--background': hasBackground
+            })}
+            style={{
+                height: height ? `${height}px` : undefined,
+                borderRadius: borderRadius ? `${borderRadius}px` : undefined,
+            }}
+        >
+            <div className="error__inner">
+                <p
+                    className={classNames({
+                        'error__inner-title': true,
+                        'error__inner-title--large': largeTitle
+                    })}
+                >
+                    {title}
+                </p>
+            </div>
+        </div>
+    )
+}
---
+++ apps/merchant-client/src/components/ui/Loader/Loader.tsx
@@ -0,0 +1,32 @@
+import { ReactComponent as IconLoading } from '../../../assets/icons/loading.svg'
+
+import classNames from 'classnames'
+
+import './Loader.scoped.scss'
+
+interface LoaderProps {
+    height?: number
+    borderRadius?: number
+    hasBackground?: boolean
+}
+
+export const Loader: React.FC<LoaderProps> = ({
+    height,
+    borderRadius,
+    hasBackground
+}) => {
+    return (
+        <div
+            className={classNames({
+                'loader': true,
+                'loader--background': hasBackground
+            })}
+            style={{
+                height: height ? `${height}px` : undefined,
+                borderRadius: borderRadius ? `${borderRadius}px` : undefined,
+            }}
+        >
+            <IconLoading className="loader__icon" />
+        </div>
+    )
+}
