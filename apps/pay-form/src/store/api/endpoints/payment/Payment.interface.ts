6a7a134137fb5f3baca5ea1f1bb2a7f499b32e6e
melonges <mellonges@icloud.com>
2023-07-15 02:16:46 +0300

    fix(pay-form): new `payment.assets` api

--- apps/pay-form/src/store/api/endpoints/payment/Payment.interface.ts
+++ apps/pay-form/src/store/api/endpoints/payment/Payment.interface.ts
@@ -1,53 +1,12 @@
-import { IPagination } from "../../types";
-import { Asset_t, Blockchain_t } from "../types";
-
-export interface IPayment {
-  id: string;
-  merchant_id: string;
-  description: string;
-  assets: IAssets;
-  evm_fee_address: string;
-  status: PaymentStatus;
-  transaction?: ITransaction;
-  // TODO: make types
-  selected_blockchain: any;
-  expires_at: string;
-  created_at: string;
-}
-
-export enum PaymentStatus {
-  pending = "pending",
-  success = "success",
-  failed = "failed",
-  inProgress = "in_progress",
-}
-interface ITransaction {
-  hash: string;
-  network: string;
-  network_id: number;
-  notification_delivered: boolean;
-}
-
-type IAssets = {
-  [key in Blockchain_t]: {
-    [key in Asset_t]: {
-      amount: string;
-      address: string;
-      fee: string;
-    };
-  };
-};
-
-export interface IGetPaymentByMerchantId extends IPagination {
-  merchant_id: string;
-}
+import { IPayment } from '@poluspay-frontend/api';
+import { Blockchain_t } from '../types';
 
 export interface IGetPaymentByPaymentId {
-  payment_id: string;
+    payment_id: string;
 }
 
 export type IGetPaymentsResponse = IPayment & {
-  blockchains: Blockchain_t[];
+    blockchains: Blockchain_t[];
 };
 
 export type Payment = IGetPaymentsResponse;
