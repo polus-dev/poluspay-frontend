b8f98006e7149bd156b18ee179a157e456cf7435
melonges <b89185236417@mail.ru>
2023-07-26 21:46:35 +0300

    feat(api): merchant brand

--- libs/api/src/lib/merchant/merchant.interface.ts
+++ libs/api/src/lib/merchant/merchant.interface.ts
@@ -1,35 +1,38 @@
 export interface IMerchantId {
-  merchant_id: string;
+    merchant_id: string;
 }
+
 export interface IMerchant {
-  created_at: string;
-  description: string;
-  display_name: string;
-  id: string;
-  domain: string;
-  domain_confirmation_code: string;
-  fail_redirect_url: string;
-  is_domain_confirmed: true;
-  is_signing_key_generated: true;
-  logo: string;
-  logo_status: LogoStatus;
-  name: string;
-  signing_key: string;
-  success_redirect_url: string;
-  webhook_url: string;
+    created_at: string;
+    description: string;
+    display_name: string;
+    id: string;
+    domain: string;
+    domain_confirmation_code: string;
+    fail_redirect_url: string;
+    is_domain_confirmed: true;
+    verified_at: string | null;
+    is_signing_key_generated: true;
+    logo: string;
+    logo_status: LogoStatus;
+    name: string;
+    signing_key: string;
+    success_redirect_url: string;
+    webhook_url: string;
 }
 
 export type LogoStatus = 'on_moderation' | 'declined' | 'confirmed' | null;
 
 export interface IPaymentMerchant
-  extends Pick<
-    IMerchant,
-    | 'description'
-    | 'display_name'
-    | 'domain'
-    | 'fail_redirect_url'
-    | 'id'
-    | 'is_domain_confirmed'
-    | 'logo'
-    | 'success_redirect_url'
-  > { }
+    extends Pick<
+        IMerchant,
+        | 'description'
+        | 'display_name'
+        | 'domain'
+        | 'fail_redirect_url'
+        | 'id'
+        | 'is_domain_confirmed'
+        | 'logo'
+        | 'success_redirect_url'
+        | 'verified_at'
+    > {}
