8fc5729c3e1459465b7d87565d091f471e2a774a
Melonges <b89185236417@mail.ru>
2023-08-03 16:37:39 +0300

    fix(merchant-client): webhook url validation

--- tools/regexp.ts
+++ tools/regexp.ts
@@ -1,5 +1,6 @@
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const httpsUrlRegex = /^https:\/\/.*/;
 export const httpUrlRegex = /^http:\/\/.*/;
+export const webhookUrlRegex =/^https?:\/\/(?:[a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,}(?:\/.*)?$/
 
 export const domainRegex = /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/;
