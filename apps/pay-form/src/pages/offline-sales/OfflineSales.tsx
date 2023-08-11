8d775096bb83144d2eb3c92345dc4091dcac2849
Nick Deev <89545245+22Crusader@users.noreply.github.com>
2023-08-11 17:22:08 +0300

    feat(pay-form): offline sales hook

--- apps/pay-form/src/pages/offline-sales/OfflineSales.tsx
+++ apps/pay-form/src/pages/offline-sales/OfflineSales.tsx
@@ -1,8 +1,17 @@
+import { useParams } from 'react-router';
+import { useOfflinePayment } from '../../hooks/useOfflinePayment'
+
 import { FormProcessing } from '../../components/pages/form/states/Processing/Processing'
 
 import './OfflineSales.scoped.scss'
 
 export const OfflineSalesPage: React.FC = () => {
+    const { id: merchantId } = useParams<{ id: string }>();
+
+    if (!merchantId) return null
+
+    const data = useOfflinePayment(merchantId)
+
     return (
         <div className="offline">
             <div className="offline__inner">
