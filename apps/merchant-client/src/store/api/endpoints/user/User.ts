709351b2c6895cf489fb65eb4682733635e06136
22Crusader <89545245+22Crusader@users.noreply.github.com>
2023-08-21 16:04:11 +0300

    fix(merchant-client): socket io removed

--- apps/merchant-client/src/store/api/endpoints/user/User.ts
+++ apps/merchant-client/src/store/api/endpoints/user/User.ts
@@ -1,7 +1,6 @@
 import type { IUserEntity } from './User.interface';
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { io } from 'socket.io-client';
 import { AuthHelper } from '../../../../logic/api';
 
 export const userApi = createApi({
