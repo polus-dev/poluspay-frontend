aac46d489854161fa05d8509fe8b113d55b67641
Melonges <b89185236417@mail.ru>
2023-10-11 12:20:42 +0300

    fix(merchant-client): import the `defaultWagmiConfig`

--- apps/merchant-client/src/utils/Web3Modal.ts
+++ apps/merchant-client/src/utils/Web3Modal.ts
@@ -1,6 +1,5 @@
 import { mainnet } from 'wagmi/chains';
-import {defaultWagmiConfig} from "@web3modal/wagmi/dist/types/exports/react";
-import {createWeb3Modal} from "@web3modal/wagmi";
+import {createWeb3Modal, defaultWagmiConfig} from "@web3modal/wagmi";
 
 const chains = [mainnet];
 export const projectId = import.meta.env.VITE_PROJECT_ID;
