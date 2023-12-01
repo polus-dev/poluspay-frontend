89e3ef9bbdd3136315bf474f8c106079143b2de7
Melonges <melonges@icloud.com>
2023-12-01 22:34:14 +0300

    feat(pay-form): redirect button in success payment view

--- apps/pay-form/src/components/pages/form/states/Success/Success.tsx
+++ apps/pay-form/src/components/pages/form/states/Success/Success.tsx
@@ -12,11 +12,14 @@ import { FormFooter } from '../../Footer/Footer';
 import { ReactComponent as IconCheckmarkRound } from '../../../../../assets/icons/checkmark-round.svg';
 
 import './Success.scoped.scss';
+import { FormButton } from '../../Button/Button';
+import { redirectToMerchantSite } from 'apps/pay-form/src/utils/redirectToMerchantSite';
 
 interface FormSuccessProps extends FormHeaderProps {}
 
 export const FormSuccess = (props: FormSuccessProps) => {
     const dispatch = useAppDispatch();
+    const {merchant: {success_redirect_url, domain}} = props, redirectUrl = success_redirect_url || domain;
 
     useEffect(() => {
         dispatch(setSmartLineStatus(SmartLineStatus.SUCCESS));
@@ -31,6 +34,14 @@ export const FormSuccess = (props: FormSuccessProps) => {
                 <IconCheckmarkRound className="success__content-icon" />
                 <p className="success__content-text">The invoice is paid</p>
             </div>
+            {redirectUrl && (
+            <div className="form__footer-button">
+                <FormButton
+                    text="Back to store"
+                    onClick={() => redirectToMerchantSite(redirectUrl)}
+                />
+            </div>
+            )}
             <div className="success__footer">
                 <FormFooter hasLegal={false} />
             </div>
